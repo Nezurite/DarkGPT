8ab077feabc268cfeb065d1db071f6d80a1b03f8
Nezurite <enfinrecup444@gmail.com>
2025-05-31 22:57:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  images: { domains: ['replicate.com'] },
-  experimental: {
-    serverActions: true,
-    optimizePackageImports: ['replicate']  # SPEED BOOST
-  }
+  images: {
+    domains: ['replicate.com'],
+  },
 }
+
 module.exports = nextConfig
